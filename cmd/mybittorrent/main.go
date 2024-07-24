8fd2e0da3b10f00065865ee75f21d32e9ebfe219
ndrpp <popandrei230@yahoo.com>
2024-07-24 21:58:38 +0300

    Get closer to decoding lists

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -9,33 +9,100 @@ import (
 	//bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
+func decodeString(i int, bencodedString string) (string, error, int) {
+	var firstColonIndex int
+	for j := i; j < len(bencodedString); j++ {
+		if bencodedString[j] == ':' {
+			firstColonIndex = j
+			break
 		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' && bencodedString[len(bencodedString)-1] == 'e' {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
 	}
+
+	lengthStr := bencodedString[i : firstColonIndex]
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+        return "", fmt.Errorf("Failed to decode string length: %v", err), 0
+	}
+	i += firstColonIndex - i + length
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil, firstColonIndex - i + length + 1
+}
+
+func decodeInteger(i int, bencodedString string) (int, error, int) {
+	var numberLen int
+    init := i
+	for j := i; j < len(bencodedString); j++ {
+		if bencodedString[j] == 'e' {
+			numberLen = j - i
+			break
+		}
+	}
+	i += numberLen
+
+	res, err := strconv.Atoi(bencodedString[init+1 : i])
+	return res, err, numberLen
+}
+
+func decodeList(i int, bencodedString string) (interface{}, error, int) {
+	result := make([]any, 0, 10)
+    var charLen int
+	for j := i; j < len(bencodedString); j++ {
+		switch {
+		case unicode.IsDigit(rune(bencodedString[j])):
+			str, err, length := decodeString(j, bencodedString)
+            //fmt.Println("str: ", str)
+			if err != nil {
+				return "", err, 0
+			}
+			result = append(result, str)
+			j += length
+
+		case bencodedString[j] == 'i':
+			num, err, length := decodeInteger(j, bencodedString)
+            //fmt.Println("num: ", num)
+			if err != nil {
+				return "", err, 0
+			}
+			result = append(result, num)
+			j += length
+
+        case bencodedString[j] == 'e':
+            charLen = j - i
+            //fmt.Println("charLen: ", charLen)
+            break
+
+        case bencodedString[j + 1] == 'l':
+            list, err, length := decodeList(j + 1, bencodedString)
+			if err != nil {
+				return "", err, 0
+			}
+			result = append(result, list)
+			j += length + 1
+
+        default:
+            continue
+		}
+	}
+
+	return result, nil, charLen
+}
+
+func decodeBencode(bencodedString string) (interface{}, error, int) {
+	for i := 0; i < len(bencodedString); i++ {
+		switch {
+		case unicode.IsDigit(rune(bencodedString[i])):
+			return decodeString(i, bencodedString)
+
+		case bencodedString[i] == 'i':
+			return decodeInteger(i, bencodedString)
+
+		case bencodedString[i] == 'l':
+			return decodeList(i, bencodedString)
+
+		default:
+			return "", fmt.Errorf("Unsupported"), 0
+		}
+	}
+	return "", fmt.Errorf("Invalid bencodedString"), 0
 }
 
 func main() {
@@ -44,7 +111,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err, _ := decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
